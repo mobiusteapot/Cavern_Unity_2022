63bd28d977d2ba15555ec4f38aa61997496b60a2
ezra <ez9002@gmail.com>
2024-10-27 16:24:09 -0400

    Fixed issue from precarious sample path use

--- Packages/com.etc.cavecavern/Samples~/CavernBIRP/Editor/CavernImportSetup.cs
+++ Packages/com.etc.cavecavern/Samples~/CavernBIRP/Editor/CavernImportSetup.cs
@@ -5,9 +5,21 @@ using UnityEditor;
 namespace ETC.CaveCavern {
     [InitializeOnLoad]
     public static class CavernSampleImportSetup {
+        // private bool IsGraphicsFoldoutOpen {
+        //     get => EditorPrefs.GetBool("Cavern_GraphicsFoldout", true);
+        //     set => EditorPrefs.SetBool("Cavern_GraphicsFoldout", value);
+        // }
+        private static bool HasSampleSceneBeenImported {
+            get => EditorPrefs.GetBool("Cavern_SampleSceneImported", false);
+            set => EditorPrefs.SetBool("Cavern_SampleSceneImported", value);
+        }
         // Check for any instances of scriptable objects that inherit from SettingsSOSingleton<T> in the project and call ValidateInPreloadedAssets on them
         static CavernSampleImportSetup()
         {
+            if (HasSampleSceneBeenImported)
+            {
+                return;
+            }
             string[] guids = AssetDatabase.FindAssets("t:SettingsSOSingleton");
             foreach (string guid in guids)
             {
