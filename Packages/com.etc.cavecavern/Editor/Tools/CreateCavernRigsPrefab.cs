2976bcdb467cdb2b28b99fe3714ce6be267da437
ezra <ez9002@gmail.com>
2024-11-03 15:41:29 -0500

    Added context menu for creating cavern, set up testing framework
    - Also renamed "Cavern Rigs" to CavernRigMain
    - Set up test case for creating the rig

---
+++ Packages/com.etc.cavecavern/Editor/Tools/CreateCavernRigsPrefab.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace ETC.CaveCavern
+{
+    public static class CreateCavernRigsPrefab
+    {
+
+        public const string CAVERN_RIGS_PREFAB_PATH = "Packages/com.etc.cavecavern/Runtime/Prefabs/CavernMainRig.prefab";
+
+        [MenuItem("GameObject/Cavern/Cavern Main Rig", false, 10)]
+        public static void CreateCavernMainRig()
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(CAVERN_RIGS_PREFAB_PATH);
+            if (prefab == null)
+            {
+                Debug.LogError("CavernRigs prefab not found at path: " + CAVERN_RIGS_PREFAB_PATH);
+                return;
+            }
+
+            GameObject instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            if (instance == null)
+            {
+                Debug.LogError("Failed to instantiate CavernRigs prefab");
+                return;
+            }
+
+            instance.name = "Cavern Main Rig";
+            Selection.activeGameObject = instance;
+        }
+    }
+}
---
+++ Packages/com.etc.cavecavern/Tests/Editor/CavernRigsCreatable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace ETC.CaveCavern.Tests
+{
+    [TestFixture]
+    [Description("Verifies that the Cavern Rig prefab can be created from the game object menu")]
+    public class CavernRigsCreatable
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // propAudioContainer = ScriptableObject.CreateInstance<PropAudioContainer>();
+            // const string assetPath = "Assets/NewScriptableObject.asset";
+            // AssetDatabase.CreateAsset(propAudioContainer, assetPath);
+            // AssetDatabase.SaveAssets();
+            // _containerGUID = AssetDatabase.GUIDFromAssetPath(assetPath);
+
+            // EditorUtility.FocusProjectWindow();
+
+            // Selection.activeObject = propAudioContainer;
+        }
+
+
+
+        [Test]
+        public void CanCreateRigsFromContextMenu()
+        {
+            // Run the CreateCavernRigsPrefab.CreateCavernMainRig method, and if any errors are thrown, fail the test
+            try
+            {
+                CreateCavernRigsPrefab.CreateCavernMainRig();
+            }
+            catch (Exception e)
+            {
+                Assert.Fail($"Failed to create Cavern Main Rig prefab: {e.Message}");
+            }
+        }
+    }
+}
