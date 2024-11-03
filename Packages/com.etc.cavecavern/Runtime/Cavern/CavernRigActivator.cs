f5a32d0eec6cade81a927b940ad8166607b54c09
ezra <ez9002@gmail.com>
2024-11-03 15:55:52 -0500

    Added more detailed error message to IsRigTypeEnabled
    - Should help with creating your own settings asset if you don't have one

--- Packages/com.etc.cavecavern/Runtime/Cavern/CavernRigActivator.cs
+++ Packages/com.etc.cavecavern/Runtime/Cavern/CavernRigActivator.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace ETC.CaveCavern{
     /// <summary>
-    /// Manages if a CavernRig is active or not based on the settings
+    /// Manages if a CavernRig is active or not based on the settings.
     /// </summary>
     public abstract class CavernRigActivator : MonoBehaviour
     {
@@ -10,7 +10,17 @@ namespace ETC.CaveCavern{
         protected virtual void Start(){
             SetCavernActive();
         }
+        /// <summary>
+        /// Checks if the Cavern Render Settings specify the rig type of this instance is enabled.
+        /// </summary>
+        /// <returns>True if it matches the current rig type in settings, false if not a match or settings are not found.</returns>
         protected bool IsRigTypeEnabled(){
+            var crs = CavernRenderSettings.Instance;
+            if(crs == null){
+                Debug.LogError("CavernRenderSettings not found. "
+                + "Please create a CavernRenderSettings asset in your asset browser, or via the Cavern section in project settings.");
+                return false;
+            }
             return CavernRenderSettings.Instance.rigType == cavernRigType;
         }
         protected virtual void SetCavernActive(){
